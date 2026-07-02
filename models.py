ad449a97d1be1ab9ca0481476baee7a8e6614b3f
spencer john <dbsspencerjohn@gmail.com>
2026-07-02 16:18:42 +0100

    add more fields to the model.py (Source : Self )

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -11,6 +12,16 @@ class VulnIssue(db.Model):
     decription = db.Column(db.Text, nullable=False)
     severity = db.Column(db.String(20), nullable=False)
     status = db.Column(db.String(20), nullable=False)
+    reporter = db.Column(db.String(100), nullable=False)
+    assigned_to = db.Column(db.String(100), nullable=False)
+    affected_system = db.Column(db.String(150), nullable=False)
+    cvss_score = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
    
     def __repr__(self):
         return f"VulnIssue(title={self.title})"
