c884d9c958876eb877466372d635714af99ee547
spencer john <dbsspencerjohn@gmail.com>
2026-07-10 14:58:57 +0100

    created a user admin and hash password for this user ( Source : Self )

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from config import Config
-from models import db
+from models import db, User
 from routes import api
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,6 +10,21 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+    # check if the user admin exist 
+    admin = User.query.filter_by(username="admin").first()
+
+    if admin is None:
+
+        admin = User(
+            username="admin",
+            password=generate_password_hash("admin123")
+        )
+
+        db.session.add(admin)
+        db.session.commit()
+
+        #print("Administrator account created.")
 
 app.register_blueprint(api)
 
