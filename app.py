d4dca35e179c1a9d9e86ccbfa3ed6dcc16c18ac9
spencer john <dbsspencerjohn@gmail.com>
2026-07-15 02:07:56 +0100

    modified the app file to remove login of admin ( Source : Self )

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from config import Config
 from models import db, User
-from routes import api, login_required
-from werkzeug.security import generate_password_hash
+from routes import api
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,27 +10,11 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-    # check if the user admin exist 
-    admin = User.query.filter_by(username="admin").first()
-
-    if admin is None:
-
-        admin = User(
-            username="admin",
-            password=generate_password_hash("admin123")
-        )
-
-        db.session.add(admin)
-        db.session.commit()
-
-        #print("Administrator account created.")
 
 app.register_blueprint(api)
 
 @app.route("/")
 def home():
-    if not login_required():
-        return redirect(url_for("api.login"))
     return render_template("index.html")
 
 
